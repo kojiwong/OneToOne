4608ec1026ecf2f7b83a40196a14cbdc324eab79
Wong <ar-koji.wong@rakuten.com>
2024-05-14 15:58:43 -0400

    reset

--- backend/calendars/admin.py
+++
@@ -1,6 +0,0 @@
-from django.contrib import admin
-# import models
-from .models import Calendar
-
-# Register your models here.
-admin.site.register(Calendar)
--- frontend/src/constants/index.js
+++
@@ -1,7 +0,0 @@
-export const API_URL = "http://localhost:8000/api/";
-
-
-export const handleCancel = () => {
-    // Add any necessary logic for canceling a dialog
-    console.log('Canceling');
-};
